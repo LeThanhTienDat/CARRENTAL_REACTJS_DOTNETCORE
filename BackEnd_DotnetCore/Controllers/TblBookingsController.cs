19ac2fef00c4ff9161bbe45423bb85ea9c698ba3
datle <datleoffice264@gmail.com>
2025-09-26 17:56:27 +0700

    third

---
+++ BackEnd_DotnetCore/Controllers/TblBookingsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackEnd_DotnetCore.Models;
+
+namespace BackEnd_DotnetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TblBookingsController : ControllerBase
+    {
+        private readonly CarRentalApiContext _context;
+
+        public TblBookingsController(CarRentalApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TblBookings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblBooking>>> GetTblBookings()
+        {
+            return await _context.TblBookings.ToListAsync();
+        }
+
+        // GET: api/TblBookings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TblBooking>> GetTblBooking(int id)
+        {
+            var tblBooking = await _context.TblBookings.FindAsync(id);
+
+            if (tblBooking == null)
+            {
+                return NotFound();
+            }
+
+            return tblBooking;
+        }
+
+        // PUT: api/TblBookings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTblBooking(int id, TblBooking tblBooking)
+        {
+            if (id != tblBooking.BookingId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tblBooking).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblBookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TblBookings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TblBooking>> PostTblBooking(TblBooking tblBooking)
+        {
+            _context.TblBookings.Add(tblBooking);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTblBooking", new { id = tblBooking.BookingId }, tblBooking);
+        }
+
+        // DELETE: api/TblBookings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTblBooking(int id)
+        {
+            var tblBooking = await _context.TblBookings.FindAsync(id);
+            if (tblBooking == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblBookings.Remove(tblBooking);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TblBookingExists(int id)
+        {
+            return _context.TblBookings.Any(e => e.BookingId == id);
+        }
+    }
+}
--- BackEnd_DotnetCore/obj/Debug/net9.0/BackEnd_DotnetCore.AssemblyInfo.cs
+++ BackEnd_DotnetCore/obj/Debug/net9.0/BackEnd_DotnetCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd_DotnetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22792bacffd8db18dc5c769f822af1b8b07f5c3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29945965bfc9e0df38a4abfb74637fbc7d9eb69d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd_DotnetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd_DotnetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
